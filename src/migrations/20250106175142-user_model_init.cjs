f566259d4411e35ce35820f63c3d2568dda81767
Jai Bhatt <jaibhatt@Jais-MacBook-Pro-14.local>
2025-01-06 23:59:18 +0530

    apis for user

---
+++ src/migrations/20250106175142-user_model_init.cjs
@@ -0,0 +1,50 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+
+      firstName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      lastName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true,
+      },
+
+      is_deleted: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ src/migrations/20250106182647-event_model_init.cjs
@@ -0,0 +1,108 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Events', {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
+      },
+      eventName: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      eventType: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      eventStartDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      eventEndDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      address_level_one: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address_level_two: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address_level_three: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address_level_four: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      address_full_string: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      organizing_entity: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      organizing_secondary_contact_email: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      organizing_poc: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      eventShortDescription: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      eventLongDescription: {
+        type: Sequelize.STRING,
+        allowNull: true,
+      },
+      eventHeroImage: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      eventProfileImage: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      pricePerUnit: {
+        type: Sequelize.FLOAT,
+        allowNull: false,
+      },
+      ticketsAvailable: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      is_deleted: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      is_active: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: true,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Events');
+  },
+};

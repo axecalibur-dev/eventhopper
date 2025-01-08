98452b74ef6a4f5fb3777919fe3a0ff446dd6aea
Jai Bhatt <jaibhatt@Jais-MacBook-Pro-14.local>
2025-01-08 15:40:22 +0530

    documentation

--- bash/start_prod.sh
+++
@@ -1,8 +0,0 @@
-echo " >> ✅  Starting with Docker Compose..."
-docker-compose -f ../docker-compose.prod.yml up -d --build
-cd ..
-mkdir src/helllo
-docker exec -it server sh -c "npx sequelize-cli db:migrate && apk update && apk add --no-cache nano htop && npm install -g pm2 && pm2 list"
-echo " >> ✅  Installed htop/pm2 and nano. Executed migrations ( check logs )."
-curl -X PUT "http://localhost:9200/events" -H "Content-Type: application/json" -d '{}'
-docker logs server -f
